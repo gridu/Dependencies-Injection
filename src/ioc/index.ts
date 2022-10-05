d6a206690ecb564ebe20e4d076c594c4bac0d0b5
Tetiana Stepanova <tetiana.stepanova@macys.com>
2022-10-05 16:22:29 +0300

    implement ioc container

--- src/ioc/index.ts
+++ src/ioc/index.ts
@@ -1,12 +1,19 @@
 import IoCContainer from 'ioc-lite';
 
-// import { Logger } from '../services/logger';
-// import { HTTP } from '../services/http';
-// import { Users } from '../services/users';
+import { Logger } from '../services/logger';
+import { HTTP } from '../services/http';
+import { Users } from '../services/users';
+
+const config = (window as any).__CONFIG__;
+delete (window as any).__CONFIG__;
+
 
 export const createIoCContainer = () =>  {
   const ioc = new IoCContainer();
-  // you can register some resources right now below...
+  ioc.registerClass('logger', Logger);
+  ioc.registerClass('http', HTTP);
+  ioc.registerClass('users', Users);
 
+  ioc.register('apiConfig', {path: config.api.path, resources: config.api.resources});
   return ioc;
 };
